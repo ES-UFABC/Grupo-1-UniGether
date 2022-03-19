0c9f292f3aa412072709948bb5b32c3289ba6347
Guilherme Rocha <GuiPython2001@gmail.com>
2022-03-19 18:48:45 -0300

    refactor: Instanciação e configuração do app express agora é feita em server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,26 @@
-import app  from './app';
+import "express-async-errors"
+import "reflect-metadata"
+import express from 'express';
+import routes from './routes/index'
+import { AppError } from './errors/AppError';
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes); 
+
+app.use((err, req, res, next) => {
+    if(err instanceof AppError){
+        return res.status(err.statusCode).json({
+            message: err.message,
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: `Internal server error ${err.message}`,
+    })
+})
 
 app.listen(8080);
