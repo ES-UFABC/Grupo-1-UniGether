abda2d15f5630e6b4249dfa5cc9705abf9374a87
Guilherme Rocha <GuiPython2001@gmail.com>
2022-03-19 18:55:30 -0300

    feat: Adição da classe UserRepository

---
+++ backend/src/app/repositories/UserRepository.js
@@ -0,0 +1,38 @@
+import User from "../models/Users";
+
+class UserRepository{
+
+    async insert(user) {
+        return await User.create(user)
+    }
+
+    async getById(id) {
+        const user = await User.findByPk(id)
+        if(user == null) throw new Error("Usuario não encontrado");
+        return user;
+    }
+
+    async getByEmail(email) {
+        const user = await User.findOne({where:{email: email}});;
+        return user;
+    }
+    
+    async getAll(){
+        return User.findAll();
+    }
+
+    async update(id, newUser){
+        const user = await User.findByPk(id);
+        if(user == null) throw new Error("Usuario não encontrado")
+        await user.update(newUser);
+        return user;
+    }
+
+    async delete(id){
+        const user = await User.findByPk(id);
+        const userIsDeleted = user.destroy().then(_ => true).catch(_ => false);
+        return userIsDeleted;
+    }
+}
+
+export {UserRepository}
