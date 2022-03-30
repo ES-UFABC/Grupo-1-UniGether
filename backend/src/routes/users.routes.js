50420aa1441a86d6d3ab885450acd78aa3f34e29
Guilherme Rocha <GuiPython2001@gmail.com>
2022-03-29 23:44:02 -0300

    refactor: Rotas de upload e download de fotos de perfil foram extraidas

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,22 +1,15 @@
-import express, { Router } from "express";
+import { Router } from "express";
 import {UserController} from "../app/controllers/UserController";
 import {ensureAuthenticated} from "../middlewares/authentication";
-import multer from "multer";
-import multerConfig from "../config/multer.config";
-import { resolve } from 'path';
 
 const userController = new UserController();
 const users = new Router();
-const upload = multer(multerConfig);
 
 users.post("/",userController.create);
 users.get("/", ensureAuthenticated, userController.index);
 users.get("/:id", ensureAuthenticated, userController.indexUser);
 users.get('/', ensureAuthenticated,userController.findByName);
 users.delete("/", ensureAuthenticated, userController.delete);
-// use abaixo na rota - upload.single("file"),
 users.put("/", ensureAuthenticated, userController.update);
-
-//users.use('/file', ensureAuthenticated, express.static(resolve(__dirname, '..', '..', '..', 'tmp', 'uploads')));
 
 export default users;
