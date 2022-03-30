faafc6f5c9d5b6f587f27eb440dda8840dc4fa13
Guilherme Rocha <GuiPython2001@gmail.com>
2022-03-29 23:53:09 -0300

    add: Cadastro das rotas '/avatars' para fotos de perfil

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import users  from "./users.routes"
 import sessions from "./session.routes"
+import avatars from "./avatar.routes"
 
 const routes = new Router()
 
 routes.use("/users", users)
 routes.use("/sessions", sessions)
+routes.use("/avatars", avatars)
 
 export default routes;
