ee8eb0180b7b7bf6b50f2d7bbca004ae2c4e19f8
Igor Milhomens <igor.milhomens@aluno.ufabc.edu.br>
2022-05-05 23:16:51 -0300

    feat: adiciondado variavel de ambiente apiURL

--- frontend/src/app/components/events/events.service.ts
+++ frontend/src/app/components/events/events.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
+
+  baseUrl = environment.apiURL
 
   constructor() { }
 }
--- frontend/src/app/components/login/login.service.ts
+++ frontend/src/app/components/login/login.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  baseUrl = environment.apiURL
 
   constructor() { }
 
